6de7c65c2667eb17ac0293c82696e9172e9c2da3
Ilshat <ilshat98981@gmail.com>
2022-08-05 09:59:51 +0500

    правит фон

--- source/js/modules/mask.js
+++ source/js/modules/mask.js
@@ -1,8 +1,40 @@
-import iMask from 'imask';
-
 const addMask = () => {
-  const inputTel = document.querySelectorAll('[data-tel]');
-  inputTel.forEach((input) => iMask(input, {mask: '+{7}(000)000-00-00'}));
+  [].forEach.call(document.querySelectorAll('[data-tel]'), (input) => {
+    let keyCode;
+
+    function Mask(event) {
+      let pos = this.selectionStart;
+      if (pos < 3) {
+        event.preventDefault();
+      }
+      let matrix = '+7 (___) ___ ____';
+      let i = 0;
+      let def = matrix.replace(/\D/g, '');
+      let val = this.value.replace(/\D/g, '');
+      let newValue = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+      i = newValue.indexOf('_');
+      if (i !== -1) {
+        newValue = newValue.slice(0, i);
+      }
+      let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function (a) {
+            return '\\d{1,' + a.length + '}';
+          }).replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        this.value = newValue;
+      }
+      if (event.type === 'blur' && this.value.length < 5) {
+        this.value = '';
+      }
+    }
+    input.addEventListener('input', Mask, false);
+    input.addEventListener('focus', Mask, false);
+    input.addEventListener('blur', Mask, false);
+    input.addEventListener('keydown', Mask, false);
+  });
 };
 
 export {addMask};
